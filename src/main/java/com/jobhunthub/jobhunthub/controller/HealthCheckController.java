564ea04bbbd360caee3861ef94653138c6902e80
Marvin Hossain <Marvin-Hossain@users.noreply.github.com>
2025-05-04 16:14:20 -0500

    Created health check controller

--- src/main/java/com/jobhunthub/jobhunthub/controller/HealthCheckController.java
+++ src/main/java/com/jobhunthub/jobhunthub/controller/HealthCheckController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HealthCheckController {
 
-    @GetMapping("/health")
+    @GetMapping("/")
     public ResponseEntity<String> healthCheck() {
         // This endpoint simply returns "OK" with a 200 status code.
         // It's designed to be fast and require no authentication.
