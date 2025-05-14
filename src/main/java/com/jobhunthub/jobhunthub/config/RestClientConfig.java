d6bf319c864058504f137bf59d8bff34698bf53d
Marvin Hossain <Marvin-Hossain@users.noreply.github.com>
2025-05-13 20:47:06 -0500

    RestClientConfig cleanup

--- src/main/java/com/jobhunthub/jobhunthub/config/RestClientConfig.java
+++ src/main/java/com/jobhunthub/jobhunthub/config/RestClientConfig.java
@@ -14,7 +14,7 @@ public class RestClientConfig {
     public RestClient restClient(RestClient.Builder builder) {
         // You can customize the builder here if needed, e.g., with base URLs, default headers, etc.
         // Example: return builder.baseUrl("https://api.example.com").build();
-        return builder.build(); // Builds a default RestClient
+        return builder.build();
     }
 }
 
