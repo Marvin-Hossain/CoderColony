e567d2ffbd85d22bfd1cd2ae6658d4198df9e822
Marvin Hossain <Marvin-Hossain@users.noreply.github.com>
2025-05-15 21:16:19 -0500

    Comment cleanup

--- src/main/java/com/jobhunthub/jobhunthub/repository/UserRepository.java
+++ src/main/java/com/jobhunthub/jobhunthub/repository/UserRepository.java
@@ -7,6 +7,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.jobhunthub.jobhunthub.model.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-    // Updated method to find a user by their provider and provider-specific ID
     Optional<User> findByProviderAndProviderId(String provider, String providerId);
 }
